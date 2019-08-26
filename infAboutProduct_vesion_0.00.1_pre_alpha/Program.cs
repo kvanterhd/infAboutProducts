2c193487be25db743acb7e9724f071523c16e6b4
Dima Halonza <kvanterhd@gmail.com>
2019-08-26 13:10:36 +0300

    nformation about different products from database

---
+++ infAboutProduct_vesion_0.00.1_pre_alpha/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace infAboutProduct_vesion_0._00._1_pre_alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product phone1 = new Product("Apple .inc", "6", "A11", "64Gb", true, 16200);
+            
+        }
+    }
+}
---
+++ infAboutProduct_vesion_0.00.1_pre_alpha/productInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace infAboutProduct_vesion_0._00._1_pre_alpha
+{
+
+
+
+    class Product
+    {
+        protected string Producer;
+        protected double Price;
+        protected string Model;
+        protected string MainProcessor;
+        protected string VRam;
+        protected bool TradeInAviability;
+        
+        public Product(string producer, string model, string mainProcessor, string vRam, bool tradeInAviability, double price)
+        {
+            Producer = producer;
+            Model = model;
+            Price = price;
+            MainProcessor = mainProcessor;
+            VRam = vRam;
+            TradeInAviability = tradeInAviability;
+            
+        }
+       
+        public virtual string GetInfo()
+        {
+            return string.Format("Producer: {0}, Price: {1}, Model : {2}, MainProcessor : {3}, VRam : {4}",TradeInAviability : {5}, Producer, Price,Model,MainProcessor,TradeInAviability);
+        }
+
+        class Table : Product
+    {
+            protected string Material;
+            protected int Legs;
+            public Table(int legs,string material,string producer, string model, string mainProcessor, string vRam, bool tradeInAviability, double price) : base(producer,model,mainProcessor,vRam,tradeInAviability,price)
+            {
+                Material = material;
+                Legs = legs;
+            }
+    }
+    class Cellphone : Product
+    {
+            protected string Features;
+            protected string Hdd;
+            public Cellphone(string features,string hdd,string producer, string model, string mainProcessor, string vRam, bool tradeInAviability, double price) : base(producer, model, mainProcessor, vRam, tradeInAviability, price)
+            {
+                Features = features;
+                Hdd = hdd;
+            }
+    }
+
+}
